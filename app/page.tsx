d8abe32d2324c1e5f444bdcc581103cfe8737d11
Harshvardhan Singh Parwal <harshparwal52@gmail.com>
2025-01-21 10:35:55 +0530

    Clek Authentication Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import {Button} from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main>
       <h1>Hello There Ai-Notion clone</h1>
+      <Button>Click Me</Button>
     </main>
   );
 }
